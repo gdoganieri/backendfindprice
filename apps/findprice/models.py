fce936d542d73a52d3423b6c3de162e406b5b547
gdoganieri <277168@studenti.unimore.it>
2021-05-25 15:42:40 +0200

    modified model for category

--- apps/findprice/models.py
+++ apps/findprice/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth import get_user_model
 # Create your models here.
 User = get_user_model()
+CATEGORY_CHOICES =  (
+    ('undefined','UNDEFINED'),
+    ('alimenti', 'ALIMENTI'),
+    ('giocattoli','GIOCATTOLI'),
+    ('elettronica','ELETTRONICA'),
+    ('casa','CASA')
+)
 
 class Product(models.Model):
     product_name= models.CharField(max_length=200, unique=True)
-    category = models.CharField(max_length=200,default= 'Undefined')
+    category = models.CharField(max_length=200,default= 'undefined', choices=CATEGORY_CHOICES)
     description = models.CharField(max_length=200)
 
     def __str__(self):
